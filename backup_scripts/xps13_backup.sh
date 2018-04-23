5bbc31093506585343164bbfa4c115263a716679
Pascal Mehnert <pascalmehnert@posteo.de>
2018-04-23 21:11:57 +0200

    Improved xps13 backup script
    - Moved timestamp and job id files from /var/spool to /var/log
    - Renamed variables for ts and job id file
    - Added pid file to prevent multiple instances running simultaneously

--- backup_scripts/xps13_backup.sh
+++ backup_scripts/xps13_backup.sh
@@ -8,11 +8,11 @@ script_file="${script_directory}/xps13_backup.sh"
 exclude_file="${script_directory}/root.exclude"
 
 log_directory="/var/log/backup_logs"
+ts_file="${log_directory}/xps13.ts"
 log_file="${log_directory}/root.log"
+job_file="${log_directory}/xps13.job"
 
-spool_directory="/var/spool/backups"
-spool_ts_file="${spool_directory}/xps13_ts"
-spool_job_file="${spool_directory}/xps13_job"
+pid_file="/var/run/xps13-backup.pid"
 
 min_backup_interval="-6 hours"
 time_reference_file=$(mktemp)
@@ -46,22 +46,20 @@ function blank_line() {
     echo "" >> $log_file
 }
 
-
 function remove_scheduling() {
-    if [[ -f "$spool_job_file" ]]; then
-        job_id=$(<$spool_job_file)
+    if [[ -f "$job_file" ]]; then
+        job_id=$(<$job_file)
         at -r "$job_id" >/dev/null 2>&1
-        rm "$spool_job_file" >/dev/null 2>&1
+        rm "$job_file" >/dev/null 2>&1
     fi
 }
 
 function add_scheduling() {
-    mkdir -p "$spool_directory"
     echo "/bin/bash $script_file" | \
         at now + 1 hour 2>&1 |      \
         tail -1 |                   \
         cut -f2 -d" " >             \
-        $spool_job_file
+        $job_file
 }
 
 
@@ -69,7 +67,7 @@ function finish() {
     exit_code=$?
 
     if [[ $exit_code -eq 0 ]]; then
-        date +%Y%m%d%H%M > $spool_ts_file
+        date +%Y%m%d%H%M > $ts_file
     fi
     
     log "Finishing backup procedure"
@@ -87,14 +85,16 @@ trap 'trap "" EXIT; terminate' \
     HUP INT QUIT TERM
 
 
+if [[ -f $pid_file ]] && kill -0 "$(cat $pid_file)" 2>/dev/null; then
+    echo "Different instance of backup already running"
+    exit 0
+fi
+
+echo $$ > $pid_file
+
 if [[ ! -d $log_directory ]]; then
     log "Creating backup log directory"
     mkdir -p $log_directory
-fi
-
-if [[ ! -d $spool_directory ]]; then
-    log "Creating spool directory"
-    mkdir -p $spool_directory
 fi
 
 remove_scheduling
@@ -102,11 +102,11 @@ remove_scheduling
 log "Starting backup procedure"
 
 
-if [[ -f $spool_ts_file ]]; then
-    touch -t "$(<$spool_ts_file)" $spool_ts_file
+if [[ -f $ts_file ]]; then
+    touch -t "$(<$ts_file)" $ts_file
     touch -d "$min_backup_interval" "$time_reference_file"
 
-    if [[ $spool_ts_file -nt $time_reference_file ]]; then
+    if [[ $ts_file -nt $time_reference_file ]]; then
         log_error "Attempting backup to soon after previous backup"
 
         exit 1
