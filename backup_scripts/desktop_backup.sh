14ebad1f1a54ca9bb3fafafc7bdcac0b777ba13a
Pascal Mehnert <pascalmehnert@posteo.de>
2018-04-28 11:56:52 +0200

    Improved desktop backup script
    - Refactored some variables and functions
    - Added pid file to prevent multiple instances running simoultaneoulsy

--- backup_scripts/desktop_backup.sh
+++ backup_scripts/desktop_backup.sh
@@ -4,14 +4,14 @@ slack_url="https://hooks.slack.com/services/T4LP4JEKW/B7L2HBM99/lwRm1s5QeUz7Zne0
 tformat="%Y-%m-%d %H:%M:%S"
 
 script_directory="/etc/backup_scripts"
-# script_file="${script_directory}/desktop_backup.sh"
+script_file="${script_directory}/desktop_backup.sh"
 exclude_file="${script_directory}/root.exclude"
 
 log_directory="/var/log/backup_logs"
+ts_file="${log_directory}/desktop.ts"
 log_file="${log_directory}/root.log"
 
-spool_directory="/var/spool/backups"
-spool_ts_file="${spool_directory}/desktop_ts"
+pid_file="/var/run/desktop-backup.pid"
 
 min_backup_interval="-6 hours"
 time_reference_file=$(mktemp)
@@ -46,18 +46,32 @@ function blank_line() {
     echo "" >> $log_file
 }
 
+function mount_backup_device() {
+    log "Mounting backup device"
+
+    if ! mount $backup_mount || ! mountpoint -q $backup_mount; then
+        log_error "Unable to mount backup device"
+        slack_message "$(hostname): Backup failed, unable to mount backup target."
+        exit 2
+    fi
+
+    unmount=true
+}
+
 function unmount_backup_device() {
     if [[ "$unmount" = true ]]; then
         log "Unmounting backup device"
         unmount $backup_mount
     fi
+
+    unmount=false
 }
 
 function finish() {
     exit_code=$?
 
     if [[ $exit_code -eq 0 ]]; then
-        date +%Y%m%d%H%M > $spool_ts_file
+        date +%Y%m%d%H%M > $ts_file
     fi
 
     unmount_backup_device
@@ -78,14 +92,16 @@ trap 'trap "" EXIT; terminate' \
     HUP INT QUIT TERM
 
 
+if [[ -f $pid_file ]] && kill -0 "$(cat $pid_file)" 2>/dev/null; then
+    echo "Different instance of backup already running"
+    exit 0
+fi
+
+echo $$ > $pid_file
+
 if [[ ! -d $log_directory ]]; then
     log "Creating backup log directory"
     mkdir -p $log_directory
-fi
-
-if [[ ! -d $spool_directory ]]; then
-    log "Creating spool directory"
-    mkdir -p $spool_directory
 fi
 
 if [[ ! -d $backup_mount ]]; then
@@ -97,11 +113,11 @@ fi
 log "Starting backup procedure"
 
 
-if [[ -f $spool_ts_file ]]; then
-    touch -t "$(<$spool_ts_file)" $spool_ts_file
+if [[ -f $ts_file ]]; then
+    touch -t "$(<$ts_file)" $ts_file
     touch -d "$min_backup_interval" "$time_reference_file"
 
-    if [[ $spool_ts_file -nt $time_reference_file ]]; then
+    if [[ $ts_file -nt $time_reference_file ]]; then
         log_error "Attempting backup to soon after previous backup"
 
         exit 1
@@ -110,15 +126,7 @@ fi
 
 
 if ! mountpoint -q $backup_mount; then
-    log "Mounting backup device"
-
-    if ! mount $backup_mount || ! mountpoint -q $backup_mount; then
-        log_error "Unable to mount backup device"
-        slack_message "$(hostname): Backup failed, unable to mount backup target."
-        exit 2
-    fi
-
-    unmount=true
+    mount_backup_device
 fi
 
 
