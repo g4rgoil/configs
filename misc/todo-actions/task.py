3d9a69c1601620d396862b67aa09672411a4c618
Pascal Mehnert <pascalmehnert@posteo.de>
2018-05-10 23:33:43 +0200

    Improved and extended task library

--- misc/todo-actions/task.py
+++ misc/todo-actions/task.py
@@ -8,22 +8,19 @@ def format_timestamp(date) -> str:
     return date.strftime("%Y-%m-%d")
 
 
-def require_no_white_space(string, msg):
+def require_no_spaces(string, msg):
     if re.search("\s", string) is not None:
         raise ValueError(msg)
 
 
 class Task(object):
     def __init__(self, description, done=False, priority=None, completed=None,
-                 created=None, projects=None, contexts=None, tags=None):
+                 created=None):
         self.done = done
         self.priority = priority
         self.completed = completed
         self.created = created
-        self.description = description
-        self.projects = projects if projects is not None else []
-        self.contexts = contexts if contexts is not None else []
-        self.key_value_tags = tags if tags is not None else {}
+        self.description = Description(description)
 
     def __str__(self):
         task = "x " if self.done else ""
@@ -33,7 +30,7 @@ class Task(object):
         task += "%s" % self.description
 
     def __repr__(self):
-        return self.__str__()
+        return "Task('" + self.__str__() + "')"
 
     @classmethod
     def parse(cls, task):
@@ -62,56 +59,42 @@ class Task(object):
             task = task[match.end():].lstrip()
 
         kwargs["description"] = task
-        kwargs["projects"] = cls.parse_projects(task)
-        kwargs["contexts"] = cls.parse_contexts(task)
-        kwargs["tags"] = cls.parse_key_value_pairs(task)
 
         return Task(**kwargs)
 
-    @classmethod
-    def parse_projects(cls, description):
-        return [e[2:] for e in re.findall(r"\s\+\S+", description)]
-
-    @classmethod
-    def parse_contexts(cls, description):
-        return [e[2:] for e in re.findall(r"\s@\S+", description)]
-
-    @classmethod
-    def parse_key_value_pairs(cls, description):
-        return dict([e.lstrip().split(":") for e in
-                     re.findall(r"\s\S+:\S+")])
-
     def do(self):
-        self.done = True
-        self.set_completion_date()
+        if not self.done:
+            self.done = True
+            self.set_completion_date()
 
     def undo(self):
         self.done = False
-        self.completed = None
+        self.unset_creation_date()
 
     def set_priority(self, priority):
+        if priority is None:
+            self.priority = None
+
+        priority = priority.upper()
         if re.fullmatch("[A-Z]", priority):
             self.priority = priority
         else:
             raise ValueError("Not a valid priority " + priority)
 
+    def unset_priority(self):
+        self.set_priority(None)
+
     def inc_priority(self):
-        new_char = chr(ord(self.priority) + 1)
-
-        if new_char <= "Z":
-            self.set_priority(new_char)
-
-    def dec_priority(self):
         new_char = chr(ord(self.priority) - 1)
 
         if new_char >= "A":
             self.set_priority(new_char)
 
-    def set_completion_date(self, date=None):
-        if date is None:
-            date = datetime.date.today()
+    def dec_priority(self):
+        new_char = chr(ord(self.priority) + 1)
 
-        self.completed = format_timestamp(date)
+        if new_char <= "Z":
+            self.set_priority(new_char)
 
     def set_creation_date(self, date=None):
         if date is None:
@@ -119,46 +102,120 @@ class Task(object):
 
         self.created = format_timestamp(date)
 
+    def unset_creation_date(self):
+        self.created = None
+
+    def set_completion_date(self, date=None):
+        if date is None:
+            date = datetime.date.today()
+
+        self.completed = format_timestamp(date)
+
+    def unset_completion_date(self):
+        self.completed = None
+
+
+class Description(object):
+
+    def __init__(self, description):
+        if not description:
+            raise ValueError("Description must not be None")
+
+        self.__description = description
+
+    def __str__(self):
+        return self.description
+
+    def __repr__(self):
+        return "Description('%s')" % self.description
+
+    def search_pattern(self, pattern):
+        return re.search(pattern, self.__description)
+
+    def has_pattern(self, pattern):
+        return self.search_pattern(pattern) is not None
+
+    def replace(self, pattern, replacement):
+        self.__description = re.sub(pattern, replacement,
+                                    self.__description)
+
+    def has_project(self, project):
+        return self.has_pattern("(\s|^)\+%s(\s|$)" % project)
+
+    def has_context(self, context):
+        return self.has_pattern("(\s|^)@%s(\s|$)" % context)
+
+    def has_kv_pair(self, key):
+        return self.has_pattern("(\s|^)%s:\S+(\s|$)" % key)
+
+    def strip(self, chars=None):
+        self.__description = self.__description.strip(chars)
+
+    def lstrip(self, chars=None):
+        self.__description = self.__description.lstrip(chars)
+
+    def rstrip(self, chars=None):
+        self.__description = self.__description.rstrip(chars)
+
+    def add_block(self, text):
+        self.rstrip()
+        self.__description += " " + text
+        self.lstrip()
+
     def add_project(self, project):
         project = project.lstrip("+")
+        require_no_spaces(project, "Project must not contain spaces")
 
-        require_no_white_space(project, "Project must not contain"
-                               "white spaces")
-
-        if project in self.projects:
+        if self.has_project(project):
             return
 
-        if self.description:
-            self.description = self.description.rstrip()
-            self.description += " "
-
-        self.description += "+" + project
-        self.projects.append(project)
+        self.add_block("+" + project)
 
     def remove_project(self, project):
         project = project.lstrip("+")
+        require_no_spaces(project, "Project must not contain spaces")
 
-        if project not in self.projects:
+        if not self.has_project(project):
             return
+
+        self.replace("(\s|^)\+%s(\s|$)" % project, " ")
+        self.strip()
 
     def add_context(self, context):
         context = context.lstrip("@")
+        require_no_spaces(context, "Context must not contain spaces")
 
-        if context in self.contexts:
+        if self.has_context(context):
             return
 
-        if self.description:
-            self.description = self.description.rstrip()
-            self.description += " "
+        self.add_block("@" + context)
 
-        self.description += "@" + context
-        self.contexts.append(context)
+    def remove_context(self, context):
+        context = context.lstrip("@")
+        require_no_spaces(context, "Context must not contain spaces")
 
-    def remove_context(self):
-        pass
+        if not self.has_context(context):
+            return
 
-    def add_key_value_pair(self):
-        pass
+        self.replace("(\s|^)@%s(\s|$)" % context, " ")
+        self.strip()
 
-    def remove_key_value_pair(self):
-        pass
+    def set_key_value_pair(self, key, value):
+        require_no_spaces(key, "Key must not contain spaces")
+        require_no_spaces(value, "Value must not contain spaces")
+
+        if not self.has_kv_pair(key):
+            self.add_block("%s:%s" % (key, value))
+        else:
+            self.replace("(\s|^)%s:\S+(\s|$)" % key,
+                         " %s:%s " % (key, value))
+            self.strip()
+
+    def remove_key_value_pair(self, key):
+        require_no_spaces(key, "Key must not contain spaces")
+
+        if not self.has_kv_pair(key):
+            return
+
+        self.replace("(\s|^)%s:\S+(\s|$)" % key, " ")
+        self.strip()
