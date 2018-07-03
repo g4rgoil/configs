b3597ea67789619cb9c32698451ec05fd0663b85
Pascal Mehnert <pascalmehnert@posteo.de>
2018-07-03 22:42:35 +0200

    Couple of minor clean ups

--- tmux/powerline-themes/mytheme.sh
+++ tmux/powerline-themes/mytheme.sh
@@ -23,24 +23,11 @@ if [ -z "$TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234 ${TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}" \
 		"hostname 33 0 ${TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}" \
-		# "vcs_branch 29 88" \
-		# "vcs_compare 60 255" \
-		# "vcs_staged 64 255" \
-		# "vcs_modified 9 255" \
-		# "vcs_others 245 0"
     )
 fi
 
 if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		# "pwd 33 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
-		# "now_playing 234 37" \
-		# "cpu 240 136" \
-		# "load 237 167" \
-		# "tmux_mem_cpu_load 234 136" \
-		# "battery 137 127" \
-		# "weather 37 255" \
-		# "date_day 235 136" \
         "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
 		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
 		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
