e407c3720bac071e1772925a30f08c4b60a61526
Pascal Mehnert <pascalmehnert@posteo.de>
2018-07-07 21:14:38 +0200

    replace exlude with pattern file in desktop backup

--- backup_scripts/desktop_backup.sh
+++ backup_scripts/desktop_backup.sh
@@ -7,7 +7,8 @@ library_file="${script_directory}/backup_library.sh"
 source $library_file
 
 # script_file="${script_directory}/desktop_backup.sh"
-exclude_file="${script_directory}/root.exclude"
+# exclude_file="${script_directory}/root.exclude"
+pattern_file="${script_directory}/root.pattern"
 
 log_directory="/var/log/backup_logs"
 ts_file="${log_directory}/desktop.ts"
