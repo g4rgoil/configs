22f6fd1d90846d1367a51200763e5d9a18831dbc
Pascal Mehnert <pascalmehnert@posteo.de>
2019-04-16 15:39:03 +0200

    Consolidated backups and script categories

--- setup/categories/category_scripts.py
+++ setup/categories/category_scripts.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 from category import Category
+from utils import require_root, get_dist
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 
 
-class CategoryBackups(Category):
-    """ Functionality for setting up scripts """
+class CategoryScripts(Category):
+    """ Functionality for setting up various scripts """
 
     directory = "scripts"
 
@@ -15,3 +16,20 @@ class CategoryBackups(Category):
 
     def set_up(self, namespace=None) -> None:
         super().set_up(namespace)
+
+    @require_root
+    def _install_dependencies(self) -> None:
+        dependencies = self.descriptor["dependencies"]
+        system_dependencies = dependencies["system"]
+        pip_dependencies = dependencies["pip"]
+
+        dist = get_dist()
+        if dist not in system_dependencies:
+            raise OSError("Cannot install dependencies: Unknown linux "
+                          "distribution '%s'" % dist)
+
+        self.utils.install_packages(dist, *system_dependencies[dist])
+        self.utils.install_pip_packages(*pip_dependencies)
+
+        if dist == "arch":
+            self.utils.run("systemctl enable --now atd".split())
